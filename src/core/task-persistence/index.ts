6241d07fcfe21e18899febff3fd1354995253d83
Stanislav Takt <st.takt@gmail.com>
2026-09-21 09:19:49 +0300

    feat(context): export task state persistence

--- src/core/task-persistence/index.ts
+++ src/core/task-persistence/index.ts
@@ -1,3 +1,5 @@
 export { type ApiMessage, readApiMessages, saveApiMessages } from "./apiMessages"
 export { readTaskMessages, saveTaskMessages } from "./taskMessages"
 export { taskMetadata } from "./taskMetadata"
+
+export { readTaskExecutionState, saveTaskExecutionState } from "./taskExecutionState" // kilocode_change
